9d7b8430743835c83f99c7eed2b3f650e125eb27
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2016-06-14 05:48:22 +0600

    Add push data to kafka

--- new_data/src/read-stream.js
+++ new_data/src/read-stream.js
@@ -1,4 +1,6 @@
 var Twitter = require('twitter');
+var Kafka = require('no-kafka');
+var producer = new Kafka.Producer({'connectionString': 'kafka:9092'});
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -15,6 +17,20 @@ console.log('TWITTER_ACCESS_TOKEN_SECRET', process.env.TWITTER_ACCESS_TOKEN_SECR
 client.stream('statuses/filter', {track: 'javascript'}, function (stream) {
   stream.on('data', function (tweet) {
     console.log(tweet.text);
+    producer.init().then(function () {
+      return producer.send({
+        topic: 'tweet',
+        partition: 0,
+        message: {
+          text: tweet.text
+        }
+      });
+    }).then(function (result) {
+      console.log('Kafka result', result);
+      /*
+       [ { topic: 'kafka-test-topic', partition: 0, offset: 353 } ]
+       */
+    });
   });
 
   stream.on('error', function (error) {
